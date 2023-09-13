c6c2a6f96ee4c80a51afa2029bcf3323bd9d7d21
Kiongnguyen <65041504+Kiongnguyen@users.noreply.github.com>
2023-09-13 15:05:00 +0300

    sua bt 6

--- bt6.js
+++ bt6.js
@@ -1,16 +1,27 @@
 // Kiểm tra xem trong 1 string có chứa tất cả các kí tự của string thứ 2 hay không.
 
+// function testStr(str1, str2) {
+//   const arrStr2 = str2.split("");
+//   let bl = false;
+//   arrStr2.forEach((tag) => {
+//     if (str1.includes(tag)) {
+//       bl = true;
+//     } else {
+//       bl = false;
+//     }
+//   });
+//   return console.log(bl);
+// }
+
 function testStr(str1, str2) {
   const arrStr2 = str2.split("");
-  let bl = false;
+  let bl = true;
   arrStr2.forEach((tag) => {
-    if (str1.includes(tag)) {
-      bl = true;
-    } else {
+    if (str1.indexOf(tag) === -1) {
       bl = false;
     }
   });
   return console.log(bl);
 }
 
-testStr("acdsfkgsd", "afkg");
+testStr("acdsfkgsd", "agafkfkfkfkfdsdkspd");
